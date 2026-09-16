3e6201f91f9874d06bfff836ae318de4bd4b750d
Thanyamai15 <27860@ud.ac.th>
2026-09-16 16:07:21 +0700

    Update input handling for total minutes and hours
    Refactor input handling for total time calculations.

--- Examination_1.py
+++ Examination_1.py
@@ -9,7 +9,6 @@
 total_minutes = int(input(60 นาที))
 total_hour = int(input( 1 ชั่วโมง))
 total_hour = int(Input( 30 นาที ))
-input_minutes = int(input(60 นาที ,1 ชั่วโมง , 30 นาที ))
-print = int(input(60 นาที))
+Output_minutes = int(input(60 นาที ,1 ชั่วโมง , 30 นาที ))
+print = int(60 นาที)
 print = int(input( 1 ชั่วโมง))
-
